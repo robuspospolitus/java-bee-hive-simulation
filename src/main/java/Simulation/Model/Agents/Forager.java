e3fbbac38c94b88006869b75c59c8746d16caf9a
karocca <klaudia.rejek@int.pl>
2026-06-03 15:15:49 +0200

    Forager &  board

--- src/main/java/Simulation/Model/Agents/Forager.java
+++ src/main/java/Simulation/Model/Agents/Forager.java
@@ -1,25 +1,60 @@
 package Simulation.Model.Agents;
 
+import Simulation.Model.MovementStrategies.AgentContext;
+import Simulation.Model.MovementStrategies.RandomMovement;
+import Simulation.Model.MovementStrategies.TargetedMovement;
+import Simulation.Model.Board;
+import Simulation.Model.BoardCells.Cell;
+
 import java.awt.*;
 
 public class Forager extends Bee {
     int numPollen;
     int carriedPollen;
     int age;
-    private Point coordinates;
 
-    Forager (int age) {
+    protected int sightRadius = 3;
+    private AgentContext movementContext;
+
+    public Forager(int age) {
         this.age = age;
-        this.coordinates;
+        this.movementContext = new AgentContext("Forager", new RandomMovement());
     }
 
-    protected void move(){
-        System.out.println ("Zbieraczka się porusza");
+    @Override
+    protected void move() {
+        movementContext.performMove();
+        System.out.println("Zbieraczka się porusza");
     }
 
-    protected Point findDestination(){
-        Point targetPosition = new Point (1,1);
-        System.out.println ("Zbieraczka znajduje destynację");
-        return targetPosition;
+    protected Point findDestination(Board board) {
+        if (carriedPollen >= 10) {
+            System.out.println("Going back to hive");
+            movementContext.setStrategy(new TargetedMovement());
+            return new Point(0, 0);
+        }
+
+        Point currentPos = this.movementContext.getPosition();
+        Cell[][] grid = board.getGrid();
+
+        for (int x = -sightRadius; x <= sightRadius; x++) {
+            for (int y = -sightRadius; y <= sightRadius; y++) {
+                int checkX = currentPos.x + x;
+                int checkY = currentPos.y + y;
+
+                if (checkX >= 0 && checkX < grid.length && checkY >= 0 && checkY < grid[0].length) {
+                    if (grid[checkX][checkY] != null && !grid[checkX][checkY].isEmpty()) {
+                        System.out.println("I'm seeing a flower");
+                        movementContext.setStrategy(new TargetedMovement());
+                        return new Point(checkX, checkY);
+                    }
+                }
+            }
+        }
+
+
+        System.out.println("I don't see anything, flying random...");
+        movementContext.setStrategy(new RandomMovement());
+        return new Point(1, 1);
     }
 }
--- src/main/java/Simulation/Model/Board.java
+++ src/main/java/Simulation/Model/Board.java
@@ -1,4 +1,20 @@
 package Simulation.Model;
 
+import Simulation.Model.BoardCells.Cell;
+
 public class Board {
+
+    private Cell[][] grid = new Cell[50][50];
+
+    public Board() {
+        for (int i = 0; i < 50; i++) {
+            for (int j = 0; j < 50; j++) {
+                this.grid[i][j] = new Cell(i, j);
+            }
+        }
+    }
+
+    public Cell[][] getGrid() {
+        return this.grid;
+    }
 }
