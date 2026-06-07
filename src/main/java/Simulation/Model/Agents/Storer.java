e71c22aa9c764fcecaa9ed86182f032bd59d0163
karocca <klaudia.rejek@int.pl>
2026-06-07 20:24:07 +0200

    Update Storer.java

--- src/main/java/Simulation/Model/Agents/Storer.java
+++ src/main/java/Simulation/Model/Agents/Storer.java
@@ -1,27 +1,38 @@
 package Simulation.Model.Agents;
 
 import Simulation.Model.Board;
+import Simulation.Model.MovementStrategies.AgentContext;
+import Simulation.Model.MovementStrategies.RandomMovement;
 
 import java.awt.*;
 
 public class Storer extends Bee{
-    private Point coordinates;
     int age;
+    int spawnX;
+    int spawnY;
+    Point spawnPosition;
 
-    public Storer (int age){
+    private AgentContext movementContext;
+
+    public Storer (int ID, int age, int spawnX, int spawnY){
+        this.ID = ID;
         this.age = age;
-        this.coordinates = coordinates;
+        this.spawnX = spawnX;
+        this.spawnY = spawnY;
+        this.spawnPosition = new Point(spawnX, spawnY);
+        this.movementContext = new AgentContext("Storer", new RandomMovement());
         totalNumBees++;
     }
 
-
+    @Override
     public void move(Board board){
+        movementContext.performMove();
         System.out.println ("Sprzątaczka się porusza");
     }
 
     protected Point findDestination(Board board){
-        Point targetPosition = new Point (1,1);
-        System.out.println ("Sprzątaczka znajduje destynację");
-        return targetPosition;
+        System.out.println ("Sprzątaczka porusza sie po ulu");
+        movementContext.setStrategy(new RandomMovement());
+        return new Point(spawnX, spawnY);
     }
 }
