ccab3e350c45d73b687730c93109601bbcf2ee53
Nadia Gill <nadia.gill2006@gmail.com>
2026-06-09 13:51:51 +0200

    refactor: make use of lookForCell method (now findClosestTarget)

--- src/main/java/Simulation/Model/Agents/Forager.java
+++ src/main/java/Simulation/Model/Agents/Forager.java
@@ -15,11 +15,8 @@ import static Simulation.Model.BoardCells.CellType.HIVE;
 import static Simulation.Model.BoardCells.CellType.POLLEN_STASH;
 
 public class Forager extends Bee {
-    int numPollen=10;
-    int carriedPollen;
-    int spawnX;
-    int spawnY;
-    Point spawnPosition;
+    private int numPollen=10, carriedPollen, spawnX, spawnY;
+    private Point spawnPosition;
 
     protected int sightRadius = 4;
     private AgentContext movementContext;
@@ -79,57 +76,42 @@ public class Forager extends Bee {
 
     protected Point findDestination(Board board) {
         Point currentPos = this.movementContext.getPosition();
-        Cell[][] grid = board.getGrid();
-
+        Cell currentCell = board.getCell(currentPos.x, currentPos.y);
 
         if (this.getEnergy() < 25.0f || carriedPollen >= 10) {
-            System.out.println("zbieraczka " + ID + " wraca do ula");
+            System.out.println("Zbieraczka " + ID + " wraca do ula");
 
-            if(currentPos.equals(board.getHiveEntrance())){
+            if (currentPos.equals(board.getHiveEntrance())) {
                 System.out.println("Teleporting");
                 movementContext.setStrategy(new TeleportMovement(board.getTeleportDestination(currentPos)));
                 return board.getTeleportDestination(currentPos);
             }
-
-            if(grid[currentPos.x][currentPos.y].getType()==CellType.MEADOW){
+            if (currentCell.getType() == CellType.MEADOW) {
                 System.out.println("Going back to hive");
                 movementContext.setStrategy(new TargetedMovement(board.getHiveEntrance()));
                 return board.getHiveEntrance();
             }
-
-            if(grid[currentPos.x][currentPos.y].getType()==CellType.HIVE){
+            if (currentCell.getType() == CellType.HIVE) {
                 System.out.println("About to stash pollen");
-                /*if(lookForCell(board, POLLEN_STASH)!=null){
-                    movementContext.setStrategy(new TargetedMovement(lookForCell(board, POLLEN_STASH)));
-                    return (lookForCell(board, POLLEN_STASH));
-                }*/
-                    movementContext.setStrategy(new TargetedMovement(board.getStashDestination(POLLEN_STASH)));
-                    return board.getStashDestination(POLLEN_STASH);
-            }
-
-        }
-
-        for (int x = -sightRadius; x <= sightRadius; x++) {
-            for (int y = -sightRadius; y <= sightRadius; y++) {
-                int checkX = currentPos.x + x;
-                int checkY = currentPos.y + y;
-
-                if (checkX >= 0 && checkX < grid.length && checkY >= 0 && checkY < grid[0].length) {
-                    if (grid[checkX][checkY] != null && !grid[checkX][checkY].isEmpty()&& grid[checkX][checkY].hasFlower()){
-                        System.out.println("I'm seeing a flower at ("+ checkX +", "+ checkY+")");
-                        movementContext.setStrategy(new TargetedMovement(new Point(checkX, checkY)));
-                        return new Point(checkX, checkY);
-                    }
-                }
+                Point stash = board.getStashDestination(CellType.POLLEN_STASH);
+                movementContext.setStrategy(new TargetedMovement(stash));
+                return stash;
             }
         }
 
+        // Looking for flowers
+        Point flower = findClosestTarget(board, CellType.MEADOW); // zakładam, że Meadow zawiera kwiaty
+        if (flower != null) {
+            return flower;
+        }
+
+        // Nothing in sight
         System.out.println("I don't see anything, flying random...");
         movementContext.setStrategy(new RandomMovement());
         return new Point(1, 1);
     }
 
-    private Point lookForCell(Board board, CellType destinationType){
+    private Point findClosestTarget(Board board, CellType typeToFind) {
         Point currentPos = this.movementContext.getPosition();
         Cell[][] grid = board.getGrid();
 
@@ -138,9 +120,10 @@ public class Forager extends Bee {
                 int checkX = currentPos.x + x;
                 int checkY = currentPos.y + y;
 
-                if (checkX >= 0 && checkX < grid.length && checkY >= 0 && checkY < grid[0].length) {
-                    if (grid[checkX][checkY] != null && !grid[checkX][checkY].isEmpty()&& grid[checkX][checkY].getType()==destinationType){
-                        System.out.println("I'm seeing a "+ destinationType+ " at ("+ checkX +", "+ checkY+")");
+                if (checkX >= 0 && checkX < board.getWidth() && checkY >= 0 && checkY < board.getHeight()) {
+                    Cell cell = grid[checkX][checkY];
+                    if (cell != null && cell.getType() == typeToFind && !cell.isEmpty()) {
+                        System.out.println("Zbieraczka " + ID + " widzi " + typeToFind + " w punkcie [" + checkX + ", " + checkY + "]");
                         movementContext.setStrategy(new TargetedMovement(new Point(checkX, checkY)));
                         return new Point(checkX, checkY);
                     }
